3ffa4436508b335345ec52412948aee7c9a748ad
mahdi <www.mahdikargar@gmail.com>
2024-06-25 17:27:50 +0330

    Bus Movement Management updated

--- Backend/models/BusMovementManagementModel.js
+++ Backend/models/BusMovementManagementModel.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose');
 const moment = require('moment-jalaali');
 
 const busMovementSchema = new mongoose.Schema({
-    busName: {type: mongoose.Schema.Types.ObjectId, ref: 'Bus', required: true},
-    origin: {type: mongoose.Schema.Types.ObjectId, ref: 'City', required: true},
-    destination: {type: mongoose.Schema.Types.ObjectId, ref: 'City', required: true},
-    days: {type: [String], required: true},
+    busName: { type: mongoose.Schema.Types.ObjectId, ref: 'Bus', required: true },
+    origin: { type: mongoose.Schema.Types.ObjectId, ref: 'City', required: true },
+    destination: { type: mongoose.Schema.Types.ObjectId, ref: 'City', required: true },
     moveDate: { type: Date, required: true },
-    moveTime: { type: Date, required: true },
-    createdAt: {type: Date, default: Date.now},
-    updatedAt: {type: Date, default: Date.now}
+    moveTime: { type: String, required: true },  // Change to String for time
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
 });
 
 const busMovementModel = mongoose.model("busMovement", busMovementSchema);
--- Backend/routes/BusMovementManagementRouter.js
+++ Backend/routes/BusMovementManagementRouter.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const BusMovementController = require("../controllers/BusMovementManagementController");
-const busmovementmodel = require("../models/BusMovementManagementModel");
 const router = express.Router();
-
+const busMovementModel=require('../models/BusMovementManagementModel')
 const busMovementController = new BusMovementController();
 
 router.post("/registerBusMovement", (req, res) => busMovementController.registerBusMovement(req, res));
 router.get('/', async (req, res) => {
     try {
-        const Drivers = await busmovementmodel.find();
+        const Drivers = await busMovementModel.find();
         res.status(200).json(Drivers);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
-});router.delete('/deleteBusMovement/:id', (req, res) => busMovementController.deleteBusMovement(req, res));
+});
+router.delete('/deleteBusMovement/:id', (req, res) => busMovementController.deleteBusMovement(req, res));
 router.patch('/updateBusMovement/:id', (req, res) => busMovementController.updateBusMovement(req, res));
 
 module.exports = router;
-
