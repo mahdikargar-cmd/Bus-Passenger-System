f04f4c9ef8bb084ac050823eb853571cbcf45465
mahdi <www.mahdikargar@gmail.com>
2024-11-18 08:12:06 +0330

    update main page - login - signUp

--- Backend/AAA/routes/AdminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userController = require('../controllers/AdminController');
-const router = express.Router();
-
-router.post('/registerAdmin', userController.registerAdmin);
-router.post('/loginAdmin', userController.loginAdmin);
-
-module.exports = router;
--- README.md
+++
@@ -1,2 +0,0 @@
-# Bus-passenger-system
-An analysis system designed and coded exclusively with MernStack
